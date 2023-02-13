61d0d07c0a0985eb3902b8b73ceca5f0ec57b042
Guillermo de la Iglesia <zguillez@gmail.com>
2023-02-13 17:02:52 +0100

    clean

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='zpy-database',
-        version='0.1.0',
+        version='0.1.1',
         author='Guillermo de la Iglesia',
         author_email='mail@zguillez.io',
         description='Pyton database wrapper',
